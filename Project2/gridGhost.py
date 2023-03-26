bc30d14c0b0e4e0bd711eeff384a62ce1e77998b
m-zalo <m.zaloziecki@gmail.com>
2023-03-26 16:30:59 +0300

    Added a grid ghost - not yet working but compiles

--- Project2/gridGhost.py
+++ Project2/gridGhost.py
@@ -2,25 +2,26 @@ from math import floor, ceil
 from ghostAgents import GhostAgent
 from util import manhattanDistance
 import numpy as np
-
+import ghostAgents
+import util
 
 class GridGhost(GhostAgent):
     """
-     A ghost that only knows the world via a Grid    """
+     A ghost that only knows the world via a Grid, but not the actual grid   """
 
     def __init__(self, index, layout=None, prob_attack=0.99, prob_flee=0.99, grid_size=5):
         GhostAgent.__init__(self, index)
         self.index = index
-        self.layout = layout
         print("Grid ghost Index: ", index)
+        self.layout = layout
         self.start = layout.agentPositions[index][1]
         print(self.start)
         self.prob_attack = prob_attack
         self.prob_flee = prob_flee
         self.grid_size = grid_size
-        self.grid = []
-        self.build_grid(grid_size, grid_size*layout.width/layout.height)
-        self.next_tile = self.start
+        self.grid = None
+        self.build_grid()
+        self.next_tile = self.position_to_grid(self.start)
 
     def getDistribution(self, state):
         ghost_state = state.getGhostState(self.index)
@@ -34,11 +35,11 @@ class GridGhost(GhostAgent):
         new_positions = [(pos[0] + a[0], pos[1] + a[1]) for a in action_vectors]
         pacman_position = state.getPacmanPosition()
         pacman_position = (round(pacman_position[0], 3), round(pacman_position[1], 3))
-        if self.is_in_node(pos):
-            self.next_node = self.find_next_node(pos, pacman_position)
+        if self.position_to_grid(pos) == self.next_tile :
+            self.next_tile = self.find_next_tile(pos, pacman_position)
         # print "current next node is ", self.next_node
         # Select best actions given the state
-        distances_to_next_node = [manhattanDistance(pos, self.next_node) for pos in new_positions]
+        distances_to_next_node = [manhattanDistance(pos, self.grid_to_position(self.next_tile)) for pos in new_positions]
         # print "distances to next nodes", distances_to_next_node
         if is_scared:
             best_score = max(distances_to_next_node)
@@ -53,20 +54,12 @@ class GridGhost(GhostAgent):
         for a in best_actions: dist[a] = best_prob / len(best_actions)
         for a in legal_actions: dist[a] += (1 - best_prob) / len(legal_actions)
         dist.normalize()
-        open('prm_edges_for_ghost_' + str(self.index) + '.txt', 'w').write(str(self.prm.edges))
-        open('prm_vertices_for_ghost_' + str(self.index) + '.txt', 'w').write(str(self.prm.vertices))
         return dist
 
-    def find_next_tile(self, pos, pacman_position):
-        next_tile = self.dfs(pos, pacman_position)
-        if next is None:
-            self.grid_size += 5
-            self.build_grid(self.grid_size, self.grid_size*self.layout.width/self.layout.height)
-            return self.next_tile
-        return next_tile
-
-    def build_grid(self, height, width):
-        if height < 1 or width < 1 :
+    def build_grid(self):                               # Create a Grid and find which tiles have obstacles to avoid
+        height = self.grid_size
+        width = self.grid_size * self.layout.width / self.layout.height
+        if height < 1 or width < 1:
             return
         self.grid = np.ones((height, width), bool)
         for i in range(height):
@@ -76,14 +69,29 @@ class GridGhost(GhostAgent):
                         if self.layout.isWall((m, n)):
                             self.grid[i, j] = False
 
-    def position_to_grid(self, pos):
+    def find_next_tile(self, pos, pacman_position):     # Call dfs on the grid to find the next tile to move to
+        next_tile = self.dfs_on_grid(pos, pacman_position)
+        if next is None:
+            self.grid_size += 5                         # If no tile is found make grid finer
+            self.build_grid()
+            return self.next_tile
+        return next_tile
+
+    def position_to_grid(self, pos):    # Find which tile contains a position on the board
         m = self.grid_size
         n = self.grid_size * self.layout.width / self.layout.height
         x = floor(pos[0] * n / self.layout.height)
         y = floor(pos[1] * m / self.layout.width)
         return x, y
 
-    def dfs(self, start, end):
+    def grid_to_position(self, pos):    # Find the positions on the board in the middle of a tile
+        m = self.grid_size
+        n = self.grid_size * self.layout.width / self.layout.height
+        x = floor((pos[0]+0.5) * self.layout.height / n)
+        y = floor((pos[1]+0.5) * self.layout.width / m)
+        return x, y
+
+    def dfs_on_grid(self, start, end):
         queue = [(end[0], end[1])]
         visited = set()
         while queue:
@@ -93,7 +101,7 @@ class GridGhost(GhostAgent):
             visited.add(v)
             if manhattanDistance(v, start) < 2:
                 return v
-            neighbor_cells = ((1,0), (0,1), (-1,0), (0,-1))
+            neighbor_cells = ((1, 0), (0, 1), (-1, 0), (0, -1))
             neighbors = list()
             for n in neighbor_cells:
                 if self.grid[v[0]+n[0], v[1]+n[1]]:
@@ -101,12 +109,5 @@ class GridGhost(GhostAgent):
             for u in neighbors:
                 if u not in visited:
                     queue.append(v)
-        print('No path found between {} and {}'.format(start, end))
+        # print('No path found between {} and {}'.format(start, end))
         return None
-
-    def not_wall(self, x, y):
-        if x < 0 or self.layout.width < x:
-            return True
-        if y < 0 or self.layout.height < y:
-            return True
-        return not self.grid[x, y]
